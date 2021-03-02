2ea22bce8cbb9ff2460ae210ecf9acd34e12e21e
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-03-02 18:02:23 -0300

    outline built

---
+++ Login/signin.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class signin extends Component {
+  render() {
+    return <div>sign in page</div>;
+  }
+}
---
+++ Todo/todo.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class todo extends Component {
+  render() {
+    return <div>todo list page</div>;
+  }
+}

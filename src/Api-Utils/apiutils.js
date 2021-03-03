b3f465d18ec5620a1df72b45111882f6130bcdaa
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-03-03 10:05:15 -0300

    finished api utils

--- src/Api-Utils/apiutils.js
+++ src/Api-Utils/apiutils.js
@@ -20,11 +20,29 @@ export async function loginUser(email, password) {
   return response.body;
 }
 
-// export async function createTodo(todo, token) {
-//   const response = await request.post(`${URL}/auth/signup`).send({
-//     todo: todo,
-//     token: token
-//   });
+export async function getTodo(token) {
+  const response = await request
+    .get(`${URL}/api/todos/`)
+    .set('Authorization', token);
 
-//   return response.body;
-// }
+  return response.body;
+}
+
+export async function createTodo(todo, token) {
+  const response = await request
+    .post(`${URL}/api/todos`)
+    .set('Authorization', token)
+    .send({
+      todo: todo
+    });
+
+  return response.body;
+}
+
+export async function completeTodo(todoId, token) {
+  const response = await request
+    .put(`${URL}/api/todos/:${todoId}`)
+    .set('Authorization', token);
+
+  return response.body;
+}
